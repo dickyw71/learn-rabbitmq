4ded44203f5a2bae0a7c91a80e0355598c279c7a
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-11-28 21:29:50 +0000

    Added webpack loaders for css and image files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash'
+import './style.css'
+import Logo from './logo.svg'
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello')
 
+    let _logo = new Image()
+    _logo.src = Logo
+
+    element.appendChild(_logo)
+    
     return element;
 }
 
